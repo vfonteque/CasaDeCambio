d4f6abe3ad79b175549a149404574f2060259a8b
Vinicius Fonteque <vfbbranco@gmail.com>
2023-04-20 14:35:42 -0300

    pausa

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import '../style.css';
+
+const btn = document.getElementById('button');
+const coin = document.getElementById('exchange');
+const BASE_URL = 'https://api.exchangerate.host/latest?base';
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  fetch(`${BASE_URL}=${coin.value}`)
+    .then()
+    .catch();
+});
